c2728db07ab825404ad889f08a6515f7e35399b4
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2024-02-05 16:32:14 -0500

    Added Smooth Scroll

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,12 +2,20 @@
 
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    const lenis = useLenis(({ scroll }) => {
+        // called every scroll
+        scroll.toExponential(1.5)
+      })
+
     return (
     <NextUIProvider>
         <NextThemesProvider attribute='class' defaultTheme='dark'>
+            <ReactLenis root>
             {children}
+            </ReactLenis>
         </NextThemesProvider>
     </NextUIProvider>
     )
