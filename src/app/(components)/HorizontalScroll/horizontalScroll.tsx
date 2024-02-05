0f41c3a518bac67605f638e7d866d1d3fde8d2db
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2024-02-05 08:14:21 -0500

    Horizontal Scroll

--- src/app/(components)/HorizontalScroll/horizontalScroll.tsx
+++ src/app/(components)/HorizontalScroll/horizontalScroll.tsx
@@ -10,9 +10,11 @@ import HTMLLogo from '/public/HTML5_Logo.svg'
 import BoostrapLogo from '/public/bootstrap-logo.svg'
 import GithubLogo from '/public/github-mark.svg'
 import QwikDaisyHomePage from '/public/QwikDaisyHomePage.png'
+import TSLogo from "/public/ts-logo-256.svg"
 import QwikLogo from 'public/qwik-logo.svg'
 import TailwindLogo from '/public/tailwind.svg'
 import DaisyUILogo from '/public/daisyui.svg'
+import JavaScriptLogo from '/public/JavaScript-logo.png'
 
 
 
@@ -33,7 +35,7 @@ export default function HorizontalSection() {
         <div ref={container} className="w-full h-[400vh]  relative ">
             <motion.div
                 style={{ x: transformX }}
-                className="w-[400vh] h-[100vh]   sticky top-0 bg-gradient-to-r from-black to-violet-950   flex flex-row justify-start pl-[25vw] items-center gap-96
+                className="w-[400vh] h-[100vh]   sticky top-0 bg-foreground  flex flex-row justify-start pl-[25vw] items-center gap-80
                 
             ">
 
@@ -45,7 +47,7 @@ export default function HorizontalSection() {
                     transition: 'all 0.8s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s'
                 }}
                 >
-                <Card className="w-96 bg-white bg-opacity-5 border-1 border-white text-white min-h-[60vh]"  >
+                <Card className="w-[90vw]  max-w-md bg-white bg-opacity-5 border-1 border-white text-background min-h-[60vh]"  >
                     <CardHeader className="flex flex-row justify-between items-center">
                         <div className="flex flex-col">
                             <h1 className="text-3xl">STAR Bank</h1>
@@ -64,7 +66,7 @@ export default function HorizontalSection() {
                                         alt="Star Bank Logo"
                                         removeWrapper
                                         isBlurred
-                                        className="border-1 border-white p-2 border-opacity-0 hover:border-opacity-100 
+                                        className="border-1 border-background p-2 border-opacity-0 hover:border-opacity-100 
                                 !transition-all !duration-250
                                 "
                                     />
@@ -112,6 +114,13 @@ export default function HorizontalSection() {
                             isBlurred
                             className="w-12 h-10"
                         />
+                        <Image
+                            src={JavaScriptLogo.src}
+                            alt="JavaScript Logo"
+                            removeWrapper
+                            isBlurred
+                            className="w-10 h-10"
+                        />
                     </CardFooter>
                 </Card>
                 </motion.div>
@@ -124,11 +133,11 @@ export default function HorizontalSection() {
                 }}
                 >
                 <Badge content="In Progress" color="warning" variant="solid" className="border-0 -translate-y-9">
-                    <Card className="w-96 bg-secondary bg-opacity-5 border-1 border-secondary text-secondary-foreground min-h-[60vh]">
+                    <Card className="w-[90vw]  max-w-md bg-secondary bg-opacity-5 border-1 border-secondary text-background min-h-[60vh]">
                         <CardHeader className="flex flex-row justify-between items-center ">
                             <h1 className="text-3xl  bg-gradient-to-r from-purple-500   to-[#FE9A03]  text-transparent bg-clip-text font-bold">qwikdaisy</h1>
-                            <Tooltip content="Go to the Github" className="text-foreground">
-                                <Button isIconOnly variant="solid" color="default" as={Link} href="https://github.com/Tmmcmasters/DaisyQwik" target="_blank" aria-label="Github">
+                            <Tooltip content="Go to the Github" className="text-foreground sticky">
+                                <Button isIconOnly className="!bg-white !border-0"    as={Link} href="https://github.com/Tmmcmasters/DaisyQwik" target="_blank" aria-label="Github">
                                     <Image
                                         src={GithubLogo.src}
                                         alt="Github Logo"
@@ -182,6 +191,13 @@ export default function HorizontalSection() {
                             <Image 
                                 src={DaisyUILogo.src}
                                 alt="Daisy UI Logo"
+                                removeWrapper
+                                isBlurred
+                                className="w-10 h-10"
+                            />
+                            <Image 
+                                src={TSLogo.src}
+                                alt="TypeScript Logo"
                                 removeWrapper
                                 isBlurred
                                 className="w-10 h-10"
