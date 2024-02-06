68733528c6882c46b1428c0e1c05347b8471487d
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2024-02-06 13:44:45 -0500

    Added some 3d effects

--- src/app/(components)/ContactMe/contactme.tsx
+++ src/app/(components)/ContactMe/contactme.tsx
@@ -4,6 +4,7 @@ import { Button, Card, CardBody, CardHeader, Input, Textarea } from "@nextui-org
 import { useRef, useState } from "react"
 import { CreateContact } from "./contactmeserver"
 import toast from "react-hot-toast"
+import { motion } from "framer-motion";
 
 
 export default function ContactMe() {
@@ -14,8 +15,40 @@ export default function ContactMe() {
     const [FirstNameErrorMessage, setFirstNameErrorMessage] = useState<string>("")
 
     const [submitDisabled, setSubmitDisabled] = useState<boolean>(false)
-    
+
     const [phoneNumberInput, setPhoneNumberInput] = useState<string>("")
+
+    const ref = useRef<HTMLDivElement>(null);
+    const [rotateX, setRotateX] = useState(0);
+    const [rotateY, setRotateY] = useState(0);
+
+    const ROTATION_RANGE = 8;
+    const HALF_ROTATION_RANGE = ROTATION_RANGE / 2;
+    
+    const handleMouseMove = (e: any) => {
+        if (!ref.current ) return;
+
+        const rect = ref.current.getBoundingClientRect();
+
+        const width = rect.width;
+        const height = rect.height;
+
+        const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
+        const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
+
+        const rY = mouseX / width - HALF_ROTATION_RANGE;
+        const rX = mouseY / height - HALF_ROTATION_RANGE;
+
+        setRotateX(rX);
+        setRotateY(rY);
+    }
+
+    const handleMouseLeave = () => {
+        if (!ref.current ) return;
+
+        setRotateX(0);
+        setRotateY(0);
+    }
 
     const handleInput = (e: any) => {
         const formattedPhoneNumber = formatPhoneNumber(e.target.value);
@@ -71,12 +104,15 @@ export default function ContactMe() {
         if (formData.firstName === "") {
             setFirstNameInvalid(true)
             setFirstNameErrorMessage("First Name is required")
+            invalid = true
         }
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(formData.email)) {
             setEmailInvalid(true)
             setEmailErrorMessage("Invalid email address")
+            invalid = true
         }
+
         if (invalid) {
             setSubmitLoading(false)
             return
@@ -110,26 +146,49 @@ export default function ContactMe() {
         <div className="flex flex-col text-foreground px-10 my-24 gap-12" data-theme="dark">
             <h1 className="text-5xl h-20 self-center">Thank you for visiting!</h1>
             <div className="flex flex-col justify-center items-center " >
-                <Card className="h-fit w-[90vw] max-w-[500px] bg-black border-1 border-foreground shadow-2xl shadow-foreground-500/50" isBlurred>
-                    <CardHeader>
+                <motion.div
+                    ref={ref}
+                    onMouseMove={handleMouseMove}
+                    onMouseLeave={handleMouseLeave}
+                    style={{
+                        transformStyle: "preserve-3d",
+                    }}
+                    animate={{
+                        rotateX: rotateX,
+                        rotateY: rotateY,
+                    }}
+                    className="w-fit h-fit flex justify-center items-center"
+                    >
+                <Card style={
+                    {
+                        transform: "translateZ(100px)",
+                        transformStyle: "preserve-3d",
+                    }
+                } 
+                className="h-fit w-[90vw] max-w-[500px] bg-black border-1 border-foreground shadow-2xl shadow-foreground-500/20" isBlurred>
+                    <CardHeader
+                        
+                    >
                         <h1 className="text-2xl font-semibold">
                             Contact Me:
                         </h1>
                     </CardHeader>
                     <CardBody className="flex flex-col justify-start items-center w-full gap-5">
-                        <Input label="Email" variant="bordered" type="email" isRequired isInvalid={EmailInvalid} errorMessage={EmailErrorMessage} onValueChange={(e) => setFormData({ ...formData, email: e })} />
+                        <Input
+                        
+                        label="Email" variant="bordered" type="email" isRequired isInvalid={EmailInvalid} errorMessage={EmailErrorMessage} onValueChange={(e) => setFormData({ ...formData, email: e })} />
                         <div className="flex justify-between items-center gap-5 w-full">
                             <Input label="First Name" type="text" variant="bordered" isRequired isInvalid={FirstNameInvalid} errorMessage={FirstNameErrorMessage} onValueChange={(e) => setFormData({ ...formData, firstName: e })} />
                             <Input label="Last Name" type="text" variant="bordered" onValueChange={(e) => setFormData({ ...formData, lastName: e })} />
                         </div>
                         <div className="flex justify-between items-center gap-5 w-full">
                             <Input label="Company" type="text" variant="bordered" onValueChange={(e) => setFormData({ ...formData, company: e })} />
-                            <Input label="Phone Number"  type="text" variant="bordered" 
-                            onChange={(e) => {
-                                handleInput(e);
-                                setFormData({ ...formData, phoneNumber: phoneNumberInput });
+                            <Input label="Phone Number" type="text" variant="bordered"
+                                onChange={(e) => {
+                                    handleInput(e);
+                                    setFormData({ ...formData, phoneNumber: phoneNumberInput });
 
-                            }}  value={phoneNumberInput}/>
+                                }} value={phoneNumberInput} />
                         </div>
                         <Textarea label="Message" type="text" variant="bordered" description="I do not share any of your information with anyone. For privacy reasons, I do not share my personal information on this site. Please feel to reach out to me through here or through other means like LinkedIn." onValueChange={(e) => setFormData({ ...formData, message: e })} />
                         <div className="w-full mt-5 ">
@@ -137,6 +196,7 @@ export default function ContactMe() {
                         </div>
                     </CardBody>
                 </Card>
+                </motion.div>
             </div>
         </div>
     )
